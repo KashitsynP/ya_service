2b8d296ea7f072fe0031ce1688ce4162f0c27d94
Кашицын Павел <kashitsyn_pg@devtrix.ru>
2025-02-12 22:42:07 +0500

    Prepare to deploy

--- ya_service/settings.py
+++ ya_service/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wrgg3i@x(n#y49vqxefsq1las)o@!7lesnyn5g!9xcn4y2b%8&'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = ['*']
 
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt.token_blacklist',
     'drf_spectacular',
     'corsheaders',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
